70f277490afc61b185e7de7c318ae914300af1a0
Zira Serrano <ziraserrano.dev@gmail.com>
2022-11-02 22:16:52 -0400

    Write out todo's in JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+//Essential TODO's:
+//accepts inputs number, operator and another number
+//should accept decimal numbers
+//stores inputs
+//recognize inputs and perform calculations
+//return a result
 
+//Optional:
+//display all input as it is being entered
+//store previous total as start of next operation
+//clear button should clear all entries
+//should prevent invalid inputs (operators next to each other, two decimal points)
